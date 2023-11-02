6fcf660939bf5e4a5b3451f9967c61526fc6b8d8
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-02 11:12:56 -0400

    Update memory.rs
    Fixed typing issues

--- src/apis/memory.rs
+++ src/apis/memory.rs
@@ -1,13 +1,16 @@
+use std::vec::Vec
 pub enum Memory {
     RAM(&mut [u8]),
     ROM(&[u8]),
     IOMMU(&[u8]),
     WIOMMU(&mut [u8])
 }
-pub struct MemoryRegion<WordSize,AddressSize> {
-    pub address: [u8; (AddressSize/8)+(AddressSize%8)];
-    pub memory: Memory;
-    pub downward: bool;
+pub struct MemoryRegion<WordSize> {
+    pub address: usize,
+    pub memory: Memory,
+    pub downward: bool
 }
-pub struct MemorySubsystem {
-    d
+pub struct MemorySubsystem<WordSize,AddressSize> {
+    pub addressable: Vec<usize>,
+    pub mmu_enabled: bool,
+    pub phys_memory_regions: Vec<MemoryRegion<WordSize,AddressSize>>

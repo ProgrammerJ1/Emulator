5e8db462152cc8040692fc93f57259149e0f904a
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-02 10:36:06 -0400

    Update memory.rs
    Added stuff

--- src/apis/memory.rs
+++ src/apis/memory.rs
@@ -1,11 +1,13 @@
-use std::boxed::Box;
-enum Memory {
+pub enum Memory {
     RAM(&mut [u8])
     ROM(&[u8])
     IOMMU(&[u8])
     WIOMMU(&mut [u8])
 }
 pub struct MemoryRegion<WordSize,AddressSize> {
-    address: [u8; AddressSize];
-    memory: Memory;
+    pub address: [u8; (AddressSize/8)+(AddressSize%8)];
+    pub memory: Memory;
+    pub downward: bool;
 }
+pub struct MemorySubsystem {
+    d

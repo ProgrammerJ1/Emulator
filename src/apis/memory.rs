820b5b77f71c53b7c94de177be31a9a79f7ad63d
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-02 08:59:31 -0400

    Update memory.rs
    Added memory types

--- src/apis/memory.rs
+++ src/apis/memory.rs
@@ -2,6 +2,8 @@ use std::boxed::Box;
 enum Memory {
     RAM(&mut [u8])
     ROM(&[u8])
+    IOMMU(&[u8])
+    WIOMMU(&mut [u8])
 }
 pub struct MemoryRegion<WordSize,AddressSize> {
     address: [u8; AddressSize];

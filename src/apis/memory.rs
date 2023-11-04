8072cfcd4cda252d2698076e07c592053b00e5fb
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-04 11:05:22 -0400

    Created lib file and fixed syntax memory issues

--- src/apis/memory.rs
+++ src/apis/memory.rs
@@ -1,16 +1,17 @@
-use std::vec::Vec
-pub enum Memory {
-    RAM(&mut [u8]),
-    ROM(&[u8]),
-    IOMMU(&[u8]),
-    WIOMMU(&mut [u8])
+use std::vec::Vec;
+pub enum Memory<'guestos> {
+    RAM(&'guestos mut [u8]),
+    ROM(&'guestos [u8]),
+    IOMMU(&'guestos [u8]),
+    WIOMMU(&'guestos mut [u8])
 }
-pub struct MemoryRegion<WordSize> {
+pub struct MemoryRegion<'guestos> {
     pub address: usize,
-    pub memory: Memory,
+    pub memory: Memory<'guestos>,
     pub downward: bool
 }
-pub struct MemorySubsystem<WordSize,AddressSize> {
+pub struct MemorySubsystem<'guestos> {
     pub addressable: Vec<usize>,
     pub mmu_enabled: bool,
-    pub phys_memory_regions: Vec<MemoryRegion<WordSize,AddressSize>>
+    pub phys_memory_regions: Vec<MemoryRegion<'guestos>>
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod api;
+pub mod apis {
+    pub mod memory;
+    pub mod system;
+}

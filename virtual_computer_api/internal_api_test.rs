766b035f322f70a9e25a976c25aa6c17e61095b7
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-18 16:25:13 -0500

    Create internal_api_test.rs
    Api test

---
+++ virtual_computer_api/internal_api_test.rs
@@ -0,0 +1,10 @@
+use virtual_computer_api;
+use rand::{rngs::ThreadRng,thread_rng};
+#[test]
+pub fn bit_operations() {
+    let mut trng=thread_rng();;
+    bit_setting(&mut trng)
+}
+pub fn bit_setting(rng: &mut ThreadRng) {
+    let mut numbers:[u64;2]=[rng.next_u64(),rng.next_u64()];
+    numbers

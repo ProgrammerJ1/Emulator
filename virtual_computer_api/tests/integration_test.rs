94d4ee835c3c1d8659553c42f9a53c648a2e6848
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-20 12:30:25 -0500

    Update integration_test.rs
    Added 0 in front

--- virtual_computer_api/tests/integration_test.rs
+++ virtual_computer_api/tests/integration_test.rs
@@ -15,7 +15,7 @@ pub fn bit_setting(rng: &mut ThreadRng) {
     let modified_bits:[u32; 3];
     let mut new_number_strings:[String; 3];
     {
-        let old_number_strings:[String; 3]=[format!("{:<64b}",numbers[0]),format!("{:<64b}",numbers[1].to_string()),format!("{:<64b}",numbers[2].to_string())];
+        let old_number_strings:[String; 3]=[format!("{:<064b}",numbers[0]),format!("{:<064b}",numbers[1].to_string()),format!("{:<064b}",numbers[2].to_string())];
         modified_bits=[63-unsafe{old_number_strings[0].rfind('0').unwrap_unchecked()} as u64,63+(63-unsafe{old_number_strings[1].rfind('0').unwrap_unchecked()}) as u64,63-unsafe{old_number_strings[0].find('1').unwrap_unchecked()} as u64];
         new_number_strings=old_number_strings.clone();
         for i in 0..2 {
@@ -27,6 +27,6 @@ pub fn bit_setting(rng: &mut ThreadRng) {
     virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[1], &mut numbers[0]);
     virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[2], &mut numbers[2]);
     for i in 0..3 {
-        assert_eq!(format!("{:<64b}",numbers[i]),new_number_strings[i]);
+        assert_eq!(format!("{:<064b}",numbers[i]),new_number_strings[i]);
     }
 }

f5d39d76b6a3b6a1e1e35edc910427f62153183a
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-20 10:17:21 -0500

    Update integration_test.rs
    Refactored cand will add assertations

--- virtual_computer_api/tests/integration_test.rs
+++ virtual_computer_api/tests/integration_test.rs
@@ -6,9 +6,9 @@ pub fn bit_operations() {
     bit_setting(&mut trng)
 }
 pub fn bit_setting(rng: &mut ThreadRng) {
-    let mut numbers:[u64;2]=[rng.next_u64(),rng.next_u64()];
+    let mut numbers:[u64;3]=[rng.next_u64(),rng.next_u64(),rng.next_u64()];
     {
-        for index in 0..2 {
+        for index in 0..3 {
             if numbers[index]==u64::MAX {
                 while numbers[index]==u64::MAX {
                     numbers[index]=rng.next_u64();
@@ -16,12 +16,21 @@ pub fn bit_setting(rng: &mut ThreadRng) {
             }
         }
     }
-    let old_number_strings=[numbers[0].to_string(),numbers[1].to_string()];
-    let modified_bits=[63-unsafe{old_number_strings[0].rfind('0').unwrap_unchecked()} as u64,63+(63-unsafe{old_number_strings[1].rfind('0').unwrap_unchecked()}) as u64];
-    let mut new_number_strings=old_number_strings.clone();
-    for i in 0..2 {
-        [old_number_strings[i].rmatch_indices('0')[0]];
+    let modified_bits:[u32; 3];
+    let mut new_number_strings:[String; 3];
+    {
+        let old_number_strings:[String; 3]=[numbers[0].to_string(),numbers[1].to_string(),numbers[2].to_string()];
+        modified_bits=[63-unsafe{old_number_strings[0].rfind('0').unwrap_unchecked()} as u64,63+(63-unsafe{old_number_strings[1].rfind('0').unwrap_unchecked()}) as u64,63-unsafe{old_number_strings[0].find('1').unwrap_unchecked()} as u64];
+        new_number_strings=old_number_strings.clone();
+        for i in 0..2 {
+            let chosen_bit=new_number_strings[i].rmatch_indices('0').next().unwrap_unchecked().0];
+            *new_number_strings[i][chosen_bit..chosen_bit]=*"1";
+        }
     }
     virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[0], &mut numbers[0]);
     virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[1], &mut numbers[0]);
+    virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[2], &mut numbers[2]);
+    for i in 0..3 {
+        //Assertations will go here
+    }
 }

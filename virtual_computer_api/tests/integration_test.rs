52529db08dbc94d9014fb6d2c6850776abcd7f3c
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-19 21:34:38 -0500

    Added test

--- virtual_computer_api/tests/integration_test.rs
+++ virtual_computer_api/tests/integration_test.rs
@@ -7,6 +7,21 @@ pub fn bit_operations() {
 }
 pub fn bit_setting(rng: &mut ThreadRng) {
     let mut numbers:[u64;2]=[rng.next_u64(),rng.next_u64()];
-    virtual_computer_api::bitoperations::BitOperations::set_bit(32, &mut numbers[0]);
-    virtual_computer_api::bitoperations::BitOperations::set_bit(68, &mut numbers[0]);
+    {
+        for index in 0..2 {
+            if numbers[index]==u64::MAX {
+                while numbers[index]==u64::MAX {
+                    numbers[index]=rng.next_u64();
+                }
+            }
+        }
+    }
+    let old_number_strings=[numbers[0].to_string(),numbers[1].to_string()];
+    let modified_bits=[63-unsafe{old_number_strings[0].rfind('0').unwrap_unchecked()} as u64,63+(63-unsafe{old_number_strings[1].rfind('0').unwrap_unchecked()}) as u64];
+    let mut new_number_strings=old_number_strings.clone();
+    for i in 0..2 {
+        [old_number_strings[i].rmatch_indices('0')[0]];
+    }
+    virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[0], &mut numbers[0]);
+    virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[1], &mut numbers[0]);
 }

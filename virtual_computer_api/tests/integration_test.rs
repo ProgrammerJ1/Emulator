04fadbd482e5b825aaa8b480635f3c48d7e80356
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-20 12:18:11 -0500

    Update integration_test.rs
    Added assetations and fixed stringification

--- virtual_computer_api/tests/integration_test.rs
+++ virtual_computer_api/tests/integration_test.rs
@@ -1,11 +1,7 @@
 use virtual_computer_api;
 use rand::{rngs::ThreadRng,thread_rng,RngCore};
-#[test]
-pub fn bit_operations() {
-    let mut trng=thread_rng();
-    bit_setting(&mut trng)
-}
 pub fn bit_setting(rng: &mut ThreadRng) {
+    let mut rng=thread_rng();
     let mut numbers:[u64;3]=[rng.next_u64(),rng.next_u64(),rng.next_u64()];
     {
         for index in 0..3 {
@@ -19,7 +15,7 @@ pub fn bit_setting(rng: &mut ThreadRng) {
     let modified_bits:[u32; 3];
     let mut new_number_strings:[String; 3];
     {
-        let old_number_strings:[String; 3]=[numbers[0].to_string(),numbers[1].to_string(),numbers[2].to_string()];
+        let old_number_strings:[String; 3]=[format!("{:<64b}",numbers[0]),format!("{:<64b}",numbers[1].to_string()),format!("{:<64b}",numbers[2].to_string())];
         modified_bits=[63-unsafe{old_number_strings[0].rfind('0').unwrap_unchecked()} as u64,63+(63-unsafe{old_number_strings[1].rfind('0').unwrap_unchecked()}) as u64,63-unsafe{old_number_strings[0].find('1').unwrap_unchecked()} as u64];
         new_number_strings=old_number_strings.clone();
         for i in 0..2 {
@@ -31,6 +27,6 @@ pub fn bit_setting(rng: &mut ThreadRng) {
     virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[1], &mut numbers[0]);
     virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[2], &mut numbers[2]);
     for i in 0..3 {
-        //Assertations will go here
+        assert_eq!(format!("{:<64b}",numbers[i]),new_number_strings[i]);
     }
 }

e2ec8c97ce64ebfb8aec1ec89645cfc3959d9d3b
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-27 13:24:49 -0500

    Update load_store.rs
    fixed spelling and added loading of words in host endian

--- virtual_computer_api/src/load_store.rs
+++ virtual_computer_api/src/load_store.rs
@@ -1,4 +1,5 @@
 use ux::{u24,i24};
+use host_context::{SystemContext,Endianness};
 use byteorder::{LittleEndian, BigEndian,ByteOrder};
 #[inline]
 pub fn load_unsigned_byte_with_host_pointer(ptr:&u8)->u8 {
@@ -23,4 +24,18 @@ pub fn load_unsigned_word_with_host_pointer_in_big_endian(ptr:&[u8])->u16 {
 #[inline]
 pub fn load_signed_word_with_host_pointer_in_big_endian(ptr:&[u8])->i16 {
     return BigEndian::read_i16(ptr);
+}
+#[inline]
+pub fn load_unsigned_word_with_host_pointer_in_host_endian(system_context:&SystemContext,ptr:&[u8])->u16 {
+    match system_context.endianness {
+        Endianness::Little=>return load_unsigned_word_with_host_pointer_in_little_endian(ptr);
+        Endianness::Big=>return load_unsigned_word_with_host_pointer_in_big_endian(ptr);
+    }
+}
+#[inline]
+pub fn load_signed_word_with_host_pointer_in_host_endian(ptr:&[u8])->i16 {
+    match system_context.endianness {
+        Endianness::Little=>return load_unsigned_word_with_host_pointer_in_little_endian(ptr);
+        Endianness::Big=>return load_unsigned_word_with_host_pointer_in_big_endian(ptr);
+    }
 }

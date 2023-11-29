a919e0d496ccaf953133c69c81daddb4d6cec745
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-28 20:28:36 -0500

    Added ptrs

--- virtual_computer_api/src/load_store.rs
+++ virtual_computer_api/src/load_store.rs
@@ -28,14 +28,14 @@ pub fn load_signed_word_with_host_pointer_in_big_endian(ptr:&[u8])->i16 {
 #[inline]
 pub fn load_unsigned_word_with_host_pointer_in_host_endian(system_context:&SystemContext,ptr:&[u8])->u16 {
     match system_context.endianness {
-        Endianness::Little=>return load_unsigned_word_with_host_pointer_in_little_endian(ptr);
-        Endianness::Big=>return load_unsigned_word_with_host_pointer_in_big_endian(ptr);
+        Endianness::Little=>return load_unsigned_word_with_host_pointer_in_little_endian(ptr),
+        Endianness::Big=>return load_unsigned_word_with_host_pointer_in_big_endian(ptr)
     }
 }
 #[inline]
-pub fn load_signed_word_with_host_pointer_in_host_endian(ptr:&[u8])->i16 {
+pub fn load_signed_word_with_host_pointer_in_host_endian(system_context:&SystemContext,ptr:&[u8])->i16 {
     match system_context.endianness {
-        Endianness::Little=>return load_unsigned_word_with_host_pointer_in_little_endian(ptr);
-        Endianness::Big=>return load_unsigned_word_with_host_pointer_in_big_endian(ptr);
+        Endianness::Little=>return load_signed_word_with_host_pointer_in_little_endian(ptr),
+        Endianness::Big=>return load_signed_word_with_host_pointer_in_big_endian(ptr)
     }
 }

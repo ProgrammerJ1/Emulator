3fa33eee59e3416301089ed078085c4aa2e192a9
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-11-28 20:30:23 -0500

    Fixed code

--- virtual_computer_api/src/host_context.rs
+++ virtual_computer_api/src/host_context.rs
@@ -6,16 +6,16 @@ pub struct SystemContext {
     pub endianess: Endianness
 }
 impl SystemContext {
-    fn new(endianness:Endianness)->Self {
+    fn new(endianess:Endianness)->Self {
         return Self {endianess}
     }
 }
 fn get_host_context()->SystemContext {
-    let endianness: Endianess;
+    let endianness: Endianness;
     if cfg!(target_endian = "big") {
-        endianess=Endianness::Big;
+        endianness=Endianness::Big;
     } else {
-        endianess=Endianness::Little;
+        endianness=Endianness::Little;
     }
     SystemContext::new(endianness)
 }

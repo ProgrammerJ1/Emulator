025eb76a1a2bfc8986f99008b4848893f412af0b
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-12-10 21:11:42 -0500

    Added format and initalize_context function

--- virtual_computer_api/src/arch/instructionset.rs
+++ virtual_computer_api/src/arch/instructionset.rs
@@ -1,9 +1,16 @@
 use std::{boxed::Box,ops::RangeInclusive};
-use bitvec::{order::Msb0,vec::BitVec};
+use bitvec::{slice::BitSlice};
 pub enum InstructionSize {
     Fixed(u64),
     Variable(RangeInclusive<u64>)
 }
 pub struct InstructionMode {
     size: InstructionSize,
+    instruction_formats: Box<[fn (&Self,bits:BitSlice)->(bool,bool)]>
+}
+impl InstructionMode {
+    pub fn initalize_context(size:InstructionSize,formats:Vec<fn (&Self,bits:BitSlice)->(bool,bool)>)->Self {
+        Self { size, instruction_formats: formats.into_boxed_slice()}
+    }
+
 }

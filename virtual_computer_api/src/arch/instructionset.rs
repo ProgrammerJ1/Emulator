f308852b6bf5445d19108be416e5ea5185b5102c
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-12-11 12:20:05 -0500

    Update instructionset.rs
    changes structures added format structure

--- virtual_computer_api/src/arch/instructionset.rs
+++ virtual_computer_api/src/arch/instructionset.rs
@@ -4,13 +4,24 @@ pub enum InstructionSize {
     Fixed(u64),
     Variable(RangeInclusive<u64>)
 }
+pub struct InstructionFormat {
+    param_sizes: Box<[usize]>,
+    pub validate: fn (&self,bits:&BitSlice)->(bool,bool,usize)
+}
+impl InstructionFormat {
+    pub fn new(param_sizes:&[usize],validator: fn (&self,bits:&BitSlice)->(bool,bool,usize))->Self {
+        Self{param_sizes,validator}
+    }
+}
 pub struct InstructionMode {
     size: InstructionSize,
-    instruction_formats: Box<[fn (&Self,bits:BitSlice)->(bool,bool)]>
+    instruction_formats: BTreeMap<u128,InstructionFormat>
 }
 impl InstructionMode {
-    pub fn initalize_context(size:InstructionSize,formats:Vec<fn (&Self,bits:BitSlice)->(bool,bool)>)->Self {
-        Self { size, instruction_formats: formats.into_boxed_slice()}
+    pub fn initalize_context(size:InstructionSize,formats:Vec<fn (&Self,bits:&BitSlice)->(bool,bool,usize)>)->Self {
+        Self { size, instruction_formats: }
     }
-
+    pub fn pattern_match(&self,bits:&BitSlice)->&I {
+        ;
+    }
 }

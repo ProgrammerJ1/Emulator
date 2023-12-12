c1bd10c0709566d2f7c543b15fc6ef72414e26f1
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-12-12 12:36:34 -0500

    Update instructionset.rs
    Added pattern matching

--- virtual_computer_api/src/arch/instructionset.rs
+++ virtual_computer_api/src/arch/instructionset.rs
@@ -1,8 +1,21 @@
 use std::{boxed::Box,ops::RangeInclusive};
 use bitvec::{slice::BitSlice};
+pub enum InstructionError {
+    EmptySet,
+    NotWithinISASize,
+    NoMatches,
+}
 pub enum InstructionSize {
-    Fixed(u64),
-    Variable(RangeInclusive<u64>)
+    Fixed(usize),
+    Variable(RangeInclusive<usize>)
+}
+impl InstructionSize {
+    pub fn within_size(&self,bits:&BitSlice)->bool {
+        match self {
+            Fixed(size)=>return size==bits.len(),
+            Variable(size_range)=>return size_range.contains(bits.len())
+        }
+    }
 }
 pub struct InstructionFormat {
     param_sizes: Box<[usize]>,
@@ -16,13 +29,31 @@ impl InstructionFormat {
 }
 pub struct InstructionMode {
     size: InstructionSize,
-    formats: BTreeMap<u128,InstructionFormat>
+    formats: Vec<InstructionFormat>
 }
 impl InstructionMode {
     pub fn initalize_context(size:InstructionSize,formats:Vec<InstructionFormats>)->Self {
         Self { size, formats}
     }
-    pub fn pattern_match(&self,bits:&BitSlice)->&I {
-        ;
+    pub fn pattern_match<'a>(&self,bits:&BitSlice)->Result<&'a InstructionFormat,InstructionError> {
+        if self.formats.is_empty() {
+            return Err(InstructionError::EmptySet);
+        } else if (!self.size.within_size(bits)) {
+            return Err(InstructionError::NotWithinISASize);
+        } else {
+            let mut best_match: Option<&'a InstructionFormat>=None;
+            for format in self.formats {
+                if (format.validate(bits)) {
+                    best_match=Some(format);
+                    if self.short_circuiting {
+                        break;
+                    }
+                }
+            }
+            match best_match {
+                Some(format)=>return Ok(format),
+                None=>return Err(InstructionError::NoMatches)
+            }
+        }
     }
 }

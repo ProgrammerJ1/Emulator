ffa8edbf5169eba501dac05ae6607de625d2a45f
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-12-13 09:50:18 -0500

    Update microarch.rs
    Started making microarchitecture

--- virtual_computer_api/src/arch/microarch.rs
+++ virtual_computer_api/src/arch/microarch.rs
@@ -1,1 +1,7 @@
-//struct Microarch
+use instructionset::InstructionMode
+pub trait Microarch<'a> {
+    //With any of implementor, just define a collection and return its reference, inline the function
+    pub fn get_isa(&self)->&'a Vec<InstructionMode>;
+    pub fn get_current_isa_mode(&self)->&'a InstructionMode;
+    pub fn set_isa_mode(&self,index: usize)->bool;
+}

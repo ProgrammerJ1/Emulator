927ffbdb44e986535055091e2297a833a6e606d0
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-12-15 08:56:54 -0500

    Update integration_test.rs
    Added clear bit test

--- virtual_computer_api/tests/integration_test.rs
+++ virtual_computer_api/tests/integration_test.rs
@@ -4,6 +4,7 @@ use rand::{rngs::ThreadRng,thread_rng,RngCore};
 pub fn bits_operations() {
     let mut rng=thread_rng();
     bit_setting_test(&mut rng);
+    bit_clearing_test(&mut rng);
 }
 pub fn bit_setting_test(rng: &mut ThreadRng) {
     let mut numbers:[u64;3]=[rng.next_u64(),rng.next_u64(),rng.next_u64()];
@@ -30,6 +31,35 @@ pub fn bit_setting_test(rng: &mut ThreadRng) {
     virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[0], &mut numbers[0]);
     virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[1], &mut numbers[0]);
     virtual_computer_api::bitoperations::BitOperations::set_bit(modified_bits[2], &mut numbers[2]);
+    for i in 0..3 {
+        assert_eq!(format!("{:<064b}",numbers[i]),new_number_strings[i]);
+    }
+}
+pub fn bit_clearing_test(rng: &mut ThreadRng) {
+    let mut numbers:[u64;3]=[rng.next_u64(),rng.next_u64(),rng.next_u64()];
+    {
+        for index in 0..3 {
+            if numbers[index]==u64::MAX {
+                while numbers[index]==u64::MAX {
+                    numbers[index]=rng.next_u64();
+                }
+            }
+        }
+    }
+    let modified_bits:[u64; 3];
+    let mut new_number_strings:[String; 3];
+    {
+        let old_number_strings:[String; 3]=[format!("{:<064b}",numbers[0]),format!("{:<064b}",numbers[1].to_string()),format!("{:<064b}",numbers[2].to_string())];
+        modified_bits=[63-unsafe{old_number_strings[0].rfind('1').unwrap_unchecked()} as u64,63+(63-unsafe{old_number_strings[1].rfind('1').unwrap_unchecked()}) as u64,63-unsafe{old_number_strings[0].find('0').unwrap_unchecked()} as u64];
+        new_number_strings=old_number_strings.clone();
+        for i in 0..2 {
+            let chosen_bit=modified_bits[i] as usize;
+            unsafe{*new_number_strings[i][chosen_bit..chosen_bit].as_mut_ptr()='0' as u8};
+        }
+    }
+    virtual_computer_api::bitoperations::BitOperations::clear_bit(modified_bits[0], &mut numbers[0]);
+    virtual_computer_api::bitoperations::BitOperations::clear_bit(modified_bits[1], &mut numbers[0]);
+    virtual_computer_api::bitoperations::BitOperations::clear_bit(modified_bits[2], &mut numbers[2]);
     for i in 0..3 {
         assert_eq!(format!("{:<064b}",numbers[i]),new_number_strings[i]);
     }
